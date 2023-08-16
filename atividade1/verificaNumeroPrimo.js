99b4af14eae605573fb325742fa51d0cc3ae88ce
Bianca Letícia Moura Silva <bianca@MacBook-Air-de-Bianca.local>
2023-08-15 22:23:54 -0300

    verificaNumeroPrimo.js

---
+++ atividade1/verificaNumeroPrimo.js
@@ -0,0 +1,31 @@
+function Primo(number) {
+  if (number <= 1) {
+    return false;
+  }
+
+  if (number <= 3) {
+    return true;
+  }
+
+  if (number % 2 === 0 || number % 3 === 0) {
+    return false;
+  }
+
+  for (let i = 5; i * i <= number; i += 6) {
+    if (number % i === 0 || number % (i + 2) === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function verificaPrimo(number) {
+  if (Primo(number)) {
+    console.log("Número primo - 1.");
+  } else {
+    console.log("Número não primo - 0.");
+  }
+}
+
+verificaPrimo(5);
